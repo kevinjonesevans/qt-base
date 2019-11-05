a5e32dee0afe0ff079bc34baeb0c5dddadb858c6
Kevin Jones-Evans <kevin.jones-evans@ngc.com>
2019-11-05 17:19:37 -0500

    Adds hdf5, makes the config work, modifies the main to make the compile work

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
-#include<iostream>
+#include <QtWidgets/qapplication.h>
+#include <QtWidgets/qpushbutton.h>
 
-using namespace std;
-
-// main function -
-// where the execution of program begins
-int main()
+int main(int argc, char** argv)
 {
-    // prints hello world
-    cout<<"Hello World";
+	QCoreApplication app(argc, argv);
+	QCoreApplication::setApplicationName("Application Example");
+	QCoreApplication::setApplicationVersion("1.0.0");
 
-    return 0;
-} 
+	QPushButton hello("Hello world!", 0);
+	hello.resize(100, 30);
+
+	hello.show();
+	return app.exec();
+}
